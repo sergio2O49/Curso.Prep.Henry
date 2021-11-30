2aaf7b7e645c4276e69b6944e1452e7edfdb72b1
sergio <velizsergio155@gmail.com>
2021-11-30 20:29:50 -0300

    solo faltan los primos

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -190,38 +190,67 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
-  for (var i = 2; i < numero; i++) {
-    if (numero%i==0){
-      return false;
+  if (numero != 0 && numero != 1){
+    for (let i = 2; i < numero; i++) {
+      if (numero%i==0){
+        return false;
+      }
+      else{
+        return true;
+      }
     }
   }
-  return true;
+  else{
+    return false;
+  }
 }
 
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
-  
+  if (valor==true){
+    return "Soy verdadero";
+  }
+  else{
+    return "Soy falso"
+  }
 }
 
 function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
-  //Escribe tu código aquí   
-  
+  //Escribe tu código aquí
+  var tablas=[];
+  for (let i = 0; i <= 10; i++) {
+    var res=6*i;
+    tablas[i]= res;    
+  }
+  return tablas ;
 }
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  
+  var cont=numero.toString().length;
+  if (cont==3){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+  var cont=0, num=numero;
+  do {
+    num=num+5;
+    cont=cont+1;
+  } while (cont<=7);
+  return num;
 }
 
 
